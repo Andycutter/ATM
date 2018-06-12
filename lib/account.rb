bf75d8e3c8357b7f7801c36333a025bfa39784c5
AndreasMN <andycutter@godmail.dk>
2018-06-12 17:02:44 +0200

    add expiring date code and test

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,13 @@
 class Account
     attr_accessor :pin_code, :balance, :account_status, :exp_date
 
+    STD_VLD_YRS = 5
+
     def initialize
         @pin_code = rand(1000..9999)
         @balance = 0
         @account_status = :active
-        @exp_date = '07/18'
+        @exp_date = Date.today.next_year(Account::STD_VLD_YRS).strftime('%m/%y')
     end
 
 end
