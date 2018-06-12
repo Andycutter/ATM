d7d9b25a49edde0f9690f566a6ab3bfdf5b7015e
AndreasMN <andycutter@godmail.dk>
2018-06-12 09:56:09 +0200

    add 'happy path' instance_double and tests and 'require Date' (thanks to Erik)

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,7 +5,14 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount)
+    def withdraw(amount, account)
+        case
+        when amount > account.balance
+            return
+        else
         @funds -= amount
+        account.balance = account.balance - amount
+        {status: true, message: 'succes', date: Date.today, amount: amount}
     end
+end
 end
