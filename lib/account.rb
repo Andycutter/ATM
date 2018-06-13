9e62ef2838f00f26a87b11b1599bfbf9c9950495
AndreasMN <andycutter@godmail.dk>
2018-06-13 09:25:40 +0200

    add balance of 0 on initialize

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,21 @@
 class Account
-    attr_accessor :pin_code, :balance, :account_status, :exp_date
+    attr_accessor :pin_code, :exp_date, :balance, :account_status
 
     STD_VLD_YRS = 5
 
     def initialize
-        @pin_code = rand(1000..9999)
+        @pin_code = generate_pin
+        @exp_date = Date.today.next_year(Account::STD_VLD_YRS).strftime('%m/%y')
         @balance = 0
         @account_status = :active
-        @exp_date = Date.today.next_year(Account::STD_VLD_YRS).strftime('%m/%y')
+        
     end
+
+    private
+
+    def generate_pin
+        rand(1000..9999)
+    end
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,14 +3,17 @@ require 'Date'
 
 describe Account do
 
-    it 'check length of pin code' do
-        number = 1234
-        number_length = Math.log10(number).to_i + 1
+    it 'check length of pin code is 4 digits' do
+        number_length = Math.log10(subject.pin_code).to_i + 1
         expect(number_length).to eq 4
     end
 
     it 'is expected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
+    end
+
+    it 'balance of 0 on initialize' do
+        expect(subject.balance).to eq 0
     end
 end
